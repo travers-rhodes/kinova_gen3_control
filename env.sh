22fb5fac994c3cab19139db4c679c2833c4b3c57
Travers Rhodes <tsr42@cornell.edu>
2020-02-03 14:03:17 -0500

    set master uri

--- env.sh
+++ env.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# set the ROS_MASTER_URI to my laptop machine (no reason to run rosmaster on the realtime computer)
+export ROS_MASTER_URI="http://helvellyn:11311"
 # source the kortex_ros_control_ws workspace
 source ~/kortex_ros_control_ws/devel/setup.bash
 exec "$@"
